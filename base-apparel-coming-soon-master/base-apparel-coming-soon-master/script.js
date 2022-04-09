4375858871487553d720714b38af6aee84c864fe
Jiemin Huang <jiehuan@ucdavis.edu>
2022-04-09 11:19:59 -0700

    address accessibility issues

--- base-apparel-coming-soon-master/base-apparel-coming-soon-master/script.js
+++ base-apparel-coming-soon-master/base-apparel-coming-soon-master/script.js
@@ -1,17 +1,15 @@
 const submitBtn = document.querySelector("#btn-arrow");
+const input = document.querySelector("#email-input");
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
-  let input = document.getElementById("email-input").value;
-  let mailformat =
+  let emailformat =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  if (String(input).toLowerCase().match(mailformat)) {
+  if (String(input.value).toLowerCase().match(emailformat)) {
     submitBtn.parentNode.classList.remove("invalid-email");
-    console.log("valid");
   } else {
     submitBtn.parentNode.classList.add("invalid-email");
-    console.log("invalid");
   }
 });
