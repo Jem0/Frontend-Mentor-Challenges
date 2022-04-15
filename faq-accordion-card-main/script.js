0b23d1c2585358077c84262c21fbd06c0902f540
Jiemin Huang <jiehuan@ucdavis.edu>
2022-04-14 21:26:29 -0700

    refactor

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -1,16 +1,16 @@
-const questions = document.querySelectorAll(".question");
+const questions = document.querySelectorAll(".question-wrapper");
 
 questions.forEach((question) =>
   question.addEventListener("click", () => {
-    // parentNode.parentNode is the question-answer-set class
-    if (question.parentNode.parentNode.classList.contains("active")) {
-      question.parentNode.parentNode.classList.toggle("active");
+    // parentNode is the question-answer-set class
+    if (question.parentNode.classList.contains("active")) {
+      question.parentNode.classList.toggle("active");
     } else {
       // only show the answer to the question that was clicked on
       questions.forEach((question) => {
-        question.parentNode.parentNode.classList.remove("active");
+        question.parentNode.classList.remove("active");
       });
-      question.parentNode.parentNode.classList.add("active");
+      question.parentNode.classList.add("active");
     }
   })
 );
