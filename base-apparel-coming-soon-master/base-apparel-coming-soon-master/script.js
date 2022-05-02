a3d458abde429708dc46d638a218a421cc859d00
Jiemin Huang <jiemin.huang8@gmail.com>
2022-05-02 14:30:34 -0700

    fix input field

--- base-apparel-coming-soon-master/base-apparel-coming-soon-master/script.js
+++ base-apparel-coming-soon-master/base-apparel-coming-soon-master/script.js
@@ -4,7 +4,7 @@ const input = document.querySelector("#email-input");
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
-  let emailformat =
+  const emailformat =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   if (String(input.value).toLowerCase().match(emailformat)) {
